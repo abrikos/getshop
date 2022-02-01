b9d1eac747d513d1c55a1022995febed83e7e9c8
abrikos <5422791+abrikos@users.noreply.github.com>
2022-02-01 16:36:34 +0900

    Create screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './app.sass';
+import Video from "./screens/Video";
+import Phone from "./screens/Phone";
+import Final from "./screens/Final";
 
 function App() {
+    const [screen, setScreen] = useState('video')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+        {screen==='video' && <Video/>}
+        {screen==='phone' && <Phone/>}
+        {screen==='final' && <Final/>}
+    </main>
   );
 }
 
